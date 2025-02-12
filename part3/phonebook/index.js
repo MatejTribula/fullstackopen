7ec40be974720bfaf23f443023cf9ba19a823cd7
mattveym <matej.tribula@gmail.com>
2025-02-12 21:51:29 +0100

    part3 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,24 +1,24 @@
 let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
+  {
+    'id': '1',
+    'name': 'Arto Hellas',
+    'number': '040-123456'
+  },
+  {
+    'id': '2',
+    'name': 'Ada Lovelace',
+    'number': '39-44-5323523'
+  },
+  {
+    'id': '3',
+    'name': 'Dan Abramov',
+    'number': '12-43-234345'
+  },
+  {
+    'id': '4',
+    'name': 'Mary Poppendieck',
+    'number': '39-23-6423122'
+  }
 ]
 
 
@@ -35,7 +35,7 @@ app.use(express.json())
 app.use(express.static('dist'))
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 morgan.token('data', function (req) { return JSON.stringify(req.body) })
@@ -43,75 +43,75 @@ morgan.token('data', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 app.get('/', (request, response) => {
-    response.send('<h1>welcome to phonebook</h1>')
+  response.send('<h1>welcome to phonebook</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
 
-    person ? response.json(person) : response.status(404).end()
+  person ? response.json(person) : response.status(404).end()
 })
 
 app.get('/info', (request, response) => {
-    const currentDate = new Date();
-    const dayOfWeek = currentDate.toLocaleString('en-US', { weekday: 'long' });
-    const month = currentDate.toLocaleString('en-US', { month: 'long' });
-    const day = currentDate.getDate();
-    const year = currentDate.getFullYear();
-    const time = currentDate.toLocaleTimeString('en-US', { timeZone: 'CET' });
+  const currentDate = new Date()
+  const dayOfWeek = currentDate.toLocaleString('en-US', { weekday: 'long' })
+  const month = currentDate.toLocaleString('en-US', { month: 'long' })
+  const day = currentDate.getDate()
+  const year = currentDate.getFullYear()
+  const time = currentDate.toLocaleTimeString('en-US', { timeZone: 'CET' })
 
-    response.send(`<div>
+  response.send(`<div>
         <p>Phonebook has info for ${persons.length} people</p>
         <p>${dayOfWeek}, ${month} ${day}, ${year} ${time} CET</p>
-    </div>`);
+    </div>`)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
 
-    response.status(204).end()
+  response.status(204).end()
 })
 
 const idGenerator = (arr) => {
-    const maxId = arr.length > 0
-        ? Math.max(...arr.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
+  const maxId = arr.length > 0
+    ? Math.max(...arr.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
 }
 
 
 app.post('/api/persons', (request, response) => {
-    let error = ''
+  let error = ''
 
-    if (!request.body.name) {
-        error = "name missing"
-    } else if (!request.body.number) {
-        error = "number missing"
-    } else if (persons.find(person => person.name === request.body.name)) {
-        error = 'person already is in your phonebook'
-    }
+  if (!request.body.name) {
+    error = 'name missing'
+  } else if (!request.body.number) {
+    error = 'number missing'
+  } else if (persons.find(person => person.name === request.body.name)) {
+    error = 'person already is in your phonebook'
+  }
 
-    if (error) {
-        return response.status(400).json({
-            error: { error }
-        })
-    }
+  if (error) {
+    return response.status(400).json({
+      error: { error }
+    })
+  }
 
-    const person = {
+  const person = {
 
-        id: idGenerator(persons),
-        name: request.body.name,
-        number: request.body.number
-    }
+    id: idGenerator(persons),
+    name: request.body.name,
+    number: request.body.number
+  }
 
-    persons = persons.concat(person)
-    response.json(person)
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 app.use(unknownEndpoint)
@@ -119,5 +119,5 @@ app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
